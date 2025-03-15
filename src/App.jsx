9181ec56fd7338c0e0f953d50335b0200dd386db
CJ Rasos <cj.rasos30@gmail.com>
2025-03-15 13:45:48 -0700

    Add Nav to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 
 import React from "react";
 import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Chatbot from "./Components/Chatbot";
-import Navbar from "./components/Navbar";
 const App = () => {
   const routes = [
     { path: "/", element: <Hero /> },
