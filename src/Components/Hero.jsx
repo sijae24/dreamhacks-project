70cd5477b5a096f0cd3f8a4321d25d1bbd1d1e05
Akira Egawa <lucian.ming.chen@gmail.com>
2025-03-15 16:37:04 -0700

    asdflkj

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,29 +2,20 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Hero = () => {
-  const scrollToChat = () => {
-    window.scrollTo({
-      top: window.innerHeight,
-      behavior: 'smooth'
-    });
-  };
-
   return (
     <div className="hero bg-base text-200 min-h-screen">
-      <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Peer to Peer</h1>
-          <h1 className="text-5xl font-bold">Chat Room</h1>
-          <p className="py-6">
-            Do you want to chat with your friends online? Connect with them here.
-          </p>
-          <button className="btn bg-red-500 hover:bg-red-800" onClick={scrollToChat}>
-            Chat Now
-          </button>
-        </div>
+    <div className="hero-content text-center">
+      <div className="max-w-md">
+        <h1 className="text-5xl font-bold">Peer to Peer</h1>
+        <h1 className="text-5xl font-bold">Chat Room</h1>
+        <p className="py-6">
+          Do you want to chat with your friends online? Connect with them here.
+        </p>
+        <button className="btn bg-red-500 hover:bg-red-800"><Link to="/chat">Chat Now</Link></button>
       </div>
     </div>
-  );
+  </div>
+  )
 }
 
 export default Hero
